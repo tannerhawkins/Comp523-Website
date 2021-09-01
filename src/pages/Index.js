4680f378a8575c685ce1d84081810054aa26eeca
tannerhawkins <tannerhawkins2703@gmail.com>
2021-09-01 19:26:44 -0400

    Added site info

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,10 +11,31 @@ const Index = () => (
         <div className="title">
           <h2 data-testid="heading" href={`#description`}>Project Description</h2>
           <p>
-            *** Project Decription Here ***
+            Ancient Path Adventures (APA) values the biblical immersion of kids, 
+            seeking to combine biblical stories and general education topics 
+            in an Augmented Interactive Education Platform (AIEP). With AIEP, 
+            APA seeks to create a new fun way for youth to engage with the Bible, 
+            because the KJV translation of the Bible may be difficult to understand. 
+            Ancient Path Adventures AR Education Aid is the mobile-friendly web app 
+            portion of the AIEP, which is accessed by scanning a QR code. The web app 
+            contains module quizzes associated with student accounts and leaderboards 
+            and provides a place for facilitators to track students’ progress, 
+            assign new modules, and award students through the program. Talpha 
+            Harris is our client and founder of APA and wants to provide engaging 
+            tools for people seeking God’s heart to understand the mysteries of 
+            His Word better. 
           </p>
         </div>
       </header>
+      <div className="title">
+        <p>
+          This site is used to display the work done by a group of UNC Computer Science students 
+          on a project assigned to them in one of their upper level COMP classes. The students have been
+          given a client to work with throughout the semester in an attempt to meet their software
+          needs. More information on the class and project, including information on how to become
+          a client for future classes, can be found <a href="https://comp523.cs.unc.edu/">here</a>
+        </p>
+      </div>
     </article>
   </Main>
 );
--- src/pages/Journal.js
+++ src/pages/Journal.js
@@ -1,7 +1,34 @@
 import React from 'react';
-
+import { useState } from 'react';
+import data from '../data/meetingNotes.json'
 import Main from '../layouts/Main';
+import styled from 'styled-components';
 
+const NewlineText = (props) => {
+  const [shown, setShown] = useState(false);
+  const date = props.notes.date
+  const text = props.notes.notes;
+  const newText = text.split('\r\n').map(str => 
+    <p style={{display: shown ? 'block' : 'none' }}>{str}</p>
+  );
+
+  const handleClick = () => {
+    setShown(!shown);
+  }
+
+  
+  return <div>
+    <StyledHeader onClick={handleClick}>{date}</StyledHeader>
+    {newText}
+  </div>;
+}
+
+const MeetingNotes = props => {
+  const notesList = data[props.meetingType].map(notes => 
+    <NewlineText notes={notes} />
+  );
+  return notesList;
+}
 
 const Journal = () => (
   <Main
@@ -10,22 +37,40 @@ const Journal = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading" href={`#journal`}>Journal</h2>
+          <h2 data-testid="heading" href={`#schedule`}>Schedule</h2>
           <p>
-            *** Journal Here ***
+            Tuesdays 4:30pm: Client Meeting on Zoom<br />
+            Wednesdays 10:00am: Mentor Meeting in Discord<br />
+            Wednesdays 2:45pm: Team Meeting in Sitterson Lobby<br />
           </p>
         </div>
       </header>
       <header>
         <div className="title">
-          <h2 data-testid="heading" href={`#schedule`}>Schedule</h2>
-          <p>
-            *** Schedule Here ***
-          </p>
+          <h2 data-testid="heading" href={`#journal`}>Journal</h2>
+          <h3>Click Date to Show Meeting Notes</h3>
+          <br />
+          <h3>Client Meeting Notes</h3>
+          <MeetingNotes meetingType="client"/>
+          <br />
+          <h3>Mentor Meeting Notes</h3>
+          <MeetingNotes meetingType="mentor"/>
+          <br />
+          <h3>Team Meeting Notes</h3>
+          <MeetingNotes meetingType="team"/>
         </div>
       </header>
     </article>
   </Main>
 );
+
+const StyledHeader = styled.h4`
+  &:hover {
+    color: grey;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`
+
 
 export default Journal;
